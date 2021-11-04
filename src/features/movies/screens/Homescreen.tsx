fbed1c00eac52ad551cce8a41ae33bfcc8663760
Benedict <jokanolaadedipupo@gmail.com>
2021-11-04 12:36:41 +0100

    navigation to details page

--- src/features/movies/screens/Homescreen.tsx
+++ src/features/movies/screens/Homescreen.tsx
@@ -1,75 +1,83 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { FlatList, View, Text, SafeAreaView, StyleSheet} from "react-native";
+import {
+  FlatList,
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import { Searchbar } from "react-native-paper";
 import useMovies from "../../../services/services";
 import { MovieInfoCard } from "../components/MovieCard";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 
-
-export const HomeScreen = () => {
-    const { data, isLoading, isSuccess } = useMovies();
-    console.log(data?.results);
+export const HomeScreen = ({ navigation }) => {
+  const { data, isLoading, isSuccess } = useMovies();
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.search}>
         <Searchbar placeholder="Search Movies" />
       </View>
       <View>
-      {isLoading && (
-                <React.Fragment>
-                    <Text>Loading...</Text>
-                </React.Fragment>
-            )}
+        {isLoading && (
+          <React.Fragment>
+            <Text>Loading...</Text>
+          </React.Fragment>
+        )}
 
-            {isSuccess && (
-                <>
-                    <Text>all posts</Text>
-                    <FlatList 
-                        data={data?.results}
-                        keyExtractor={(item) => `${item.id}`}
-                        renderItem={({ item }) => 
-                        (
-                            <Card elevation={5} style={styles.card}>
-                            <Card.Cover
-                              style={styles.cover}
-                              source={{ uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}` }}
-                            />
-                            <Card.Content style={styles.cardcontent}>
-                              <Title>{item.title}</Title>
-                              {/* <Paragraph>{item.}</Paragraph> */}
-                            </Card.Content>
-                          </Card>
-                        )
-                    }
+        {isSuccess && (
+          <>
+            <Text>all posts</Text>
+            <FlatList
+              data={data?.results}
+              keyExtractor={(item) => `${item.id}`}
+              renderItem={({ item }) => (
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate("MovieDetails", { id: item.id })
+                  }
+                >
+                  <Card elevation={5} style={styles.card}>
+                    <Card.Cover
+                      style={styles.cover}
+                      source={{
+                        uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}`,
+                      }}
                     />
-                </>
-            )}
+                    <Card.Content style={styles.cardcontent}>
+                      <Title>{item.title}</Title>
+                      <Paragraph>{item.release_date}</Paragraph>
+                    </Card.Content>
+                  </Card>
+                </TouchableOpacity>
+              )}
+            />
+          </>
+        )}
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-    card: {
-      marginTop: 15,
-      backgroundColor: '#fff'
-    },
-    cover: {
-      padding: 5,
-    },
-    cardcontent: {
-      marginTop: 5,
-      marginBottom: -5,
-    },
-    container: {
-      flex: 1,
-      backgroundColor: 'transparent'
-    },
-    search: {
-      padding: 10,
-    },
-  });
-
-
-
+  card: {
+    marginTop: 15,
+    backgroundColor: "#fff",
+  },
+  cover: {
+    padding: 5,
+  },
+  cardcontent: {
+    marginTop: 5,
+    marginBottom: -5,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "transparent",
+  },
+  search: {
+    padding: 10,
+  },
+});
--- src/services/services.ts
+++ src/services/services.ts
@@ -2,12 +2,13 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import { URL, API_KEY } from "../../constant";
 
-const getMovies = async () => {
+const getMovies = async (movieId) => {
   const {data} = (await axios.get(
     `https://api.themoviedb.org/3/discover/movie?api_key=fa2fa335b93284c5277907e139256806`
   )) as any;
   return data;
 };
 
-const useMovies = () => useQuery("movies", getMovies);
+const useMovies = (movieId) => useQuery(["movies", movieId],  () => getMovies(movieId));
 export default useMovies;
+
